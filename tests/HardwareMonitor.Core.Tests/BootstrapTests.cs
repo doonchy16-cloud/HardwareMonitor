adce7ebfe5c7fbc58189d36d6af95285cadacde8
doonchy16-cloud <doonchy16@gmail.com>
2026-08-22 00:14:54 -0700

    chore: establish .NET 10 CI foundation

---
+++ tests/HardwareMonitor.Core.Tests/BootstrapTests.cs
@@ -0,0 +1,10 @@
+namespace TheSpark.HardwareMonitor.Core.Tests;
+
+public sealed class BootstrapTests
+{
+    [Fact]
+    public void Test_runner_is_operational()
+    {
+        Assert.True(true);
+    }
+}
