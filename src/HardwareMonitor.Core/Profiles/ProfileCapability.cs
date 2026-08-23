c0ec669d56a70e4b037391396af32e0aefb8a3b3
doonchy16-cloud <doonchy16@gmail.com>
2026-08-22 23:16:45 -0700

    feat: add profile capabilities

---
+++ src/HardwareMonitor.Core/Profiles/ProfileCapability.cs
@@ -0,0 +1,17 @@
+namespace TheSpark.HardwareMonitor.Core.Profiles;
+
+public enum ProfileCapability
+{
+    ViewProfiles,
+    PublishHardwareTelemetry,
+    PublishDevicePresence,
+    PublishLimitedClientTelemetry,
+    TrainingMode,
+    ManageProfiles,
+    ManageDevices,
+    ManageAlerts,
+    ManageRemoteAccess,
+    ReceiveNotifications,
+    ViewDiagnostics,
+    ViewHistory
+}
