06b77c5933d7a633698a63a2ee1d5c19731465fa
doonchy16-cloud <doonchy16@gmail.com>
2026-08-23 10:54:07 -0700

    feat: add profile registry editor surface

---
+++ src/HardwareMonitor.Core/Profiles/ProfileRegistryEditor.cs
@@ -0,0 +1,10 @@
+namespace TheSpark.HardwareMonitor.Core.Profiles;
+
+public static class ProfileRegistryEditor
+{
+    public static ProfileRegistryDocument Upsert(ProfileRegistryDocument registry, MonitoringProfile profile) =>
+        throw new NotImplementedException();
+
+    public static ProfileRegistryDocument Remove(ProfileRegistryDocument registry, Guid profileId) =>
+        throw new NotImplementedException();
+}
